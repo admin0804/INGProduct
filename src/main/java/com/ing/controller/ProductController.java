571fbc55441e512cde4d1f05964805dcbcba7cb9
hariharamohanty@gmail.com <hariharamohanty@gmail.com>
2019-04-11 00:16:05 -0700

    Update ProductController.java

--- src/main/java/com/ing/controller/ProductController.java
+++ src/main/java/com/ing/controller/ProductController.java
@@ -1,5 +1,22 @@
 package com.ing.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin
 public class ProductController {
+	
+	public static final String WELCOME = "Welcome to ING Product Applications";
+	
+	@GetMapping("/overview")
+	public ResponseEntity<String> welcomeMessage(){
+		return new ResponseEntity<>(WELCOME,HttpStatus.OK);
+	}
 
 }
